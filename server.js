efb4721fafdebdee67d15f02f296a47bc3613f95
Eva Nikolaieva <draculahgirl@gmail.com>
2023-05-10 22:07:41 +0200

    update server.js
    add script for localhost:8080
    and localhost:8080/documentation

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const http = require('http');
+const fs = require('fs');
 const url = require('url');
 
-let addr = 'http://localhost:8080/documentation';
-let q = url.parse(addr, true);
-
-console.log(q.host); // returns 'localhost:8080'
-console.log(q.pathname); // returns '/default.html'
-
-
 http.createServer((request, response) => {
+  let addr = 'http://localhost:8080/documentation';
+  let q = url.parse(addr, true);
+  filePath = '';
+
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if(err) {
+      throw err;
+    }
+  
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello Node!\n');
+    response.write(data);
+    response.end('Hello Node!');
+  });
+
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
+
