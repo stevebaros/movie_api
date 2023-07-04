ac33dfcdb3da33065658174b42cb4ba744bfda27
Eva Nikolaieva <draculahgirl@gmail.com>
2023-07-05 01:03:12 +0200

    update index.js

--- index.js
+++ index.js
@@ -2,146 +2,28 @@ const express = require("express");
 bodyParser = require('body-parser'),
   uuid = require('uuid');
 
+const morgan = require("morgan");
 const app = express();
-morgan = require("morgan");
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
+
+mongoose.connect('mongodb://localhost:27017/cfDB'),
+
 app.use(bodyParser.json());
 
+//log requests to server
+app.use(morgan("common"));
 
-let movies = [
-  {
-    "Title": "Stealing Beauty",
-    "director": {
-      "Name": "Bernardo Bertolucci",
-      "Birth Date": 1941
-    },
-    "Genre": {
-      "Name": "Drama",
-      "description": "A young American comes to Italy for vacation to find the key to her mother`s mystery"
-    },
-    "ImageURL": "https://pics.filmaffinity.com/stealing_beauty_io_ballo_da_sola-799619154-mmed.jpg"
-  },
-  {
-    "Title": "Ultimo Tango a Parigi",
-    "director": {
-      "Name": "Bernardo Bertolucci",
-      "Birth Date": 1941
-    },
-    "Genre": {
-      "Name": "Drama",
-      "description": "description"
-    },
-    "ImageURL": "url"
-  },
-  {
-    "Title": "The Dreamers",
-    "director": {
-      "Name": "Bernardo Bertolucci",
-      "Birth Date": 1941
-    },
-    "Genre": {
-      "Name": "Drama",
-      "description": "The Dreamers is a meditation on youth and art — the ways in which life and art become conflated, how art becomes a part of us, and how we, in our youthful idealism, transform those we love into art"
-    },
-    "ImageURL": "url"
-  },
-  {
-    "Title": "Little Buddha",
-    "director": {
-      "Name": "Bernardo Bertolucci",
-      "Birth Date": 1941
-    },
-    "Genre": {
-      "Name": "Drama",
-      "description": "Tibetan Buddhist monks from a monastery in Bhutan, led by Lama Norbu, are searching for a child who is the rebirth of a great Buddhist teacher, Lama Dorje"
-    },
-    "ImageURL": "url"
-  },
-  {
-    "Title": "Persona",
-    "director": {
-      "Name": "Ingmar Bergman",
-      "Birth Date": 1918
-    },
-    "Genre": {
-      "Name": "Drama",
-      "description": "description"
-    },
-    "ImageURL": "url"
-  },
-  {
-    "Title": "Viskningar och rop",
-    "director": {
-      "Name": "Ingmar Bergman",
-      "Birth Date": 1918
-    },
-    "Genre": {
-      "Name": "Drama",
-      "description": "description"
-    },
-    "ImageURL": "url"
-  },
-  {
-    "Title": "Smultronstället",
-    "director": {
-      "Name": "Ingmar Bergman",
-      "Birth Date": 1918
-    },
-    "Genre": {
-      "Name": "Drama",
-      "description": "description"
-    },
-    "ImageURL": "url"
-  },
-  {
-    "Title": "La notte",
-    "director": {
-      "Name": "Michelangelo Antonioni",
-      "Birth Date": 1912,
-    },
-    "Genre": {
-      "Name": "Drama",
-      "description": "description"
-    },
-    "ImageURL": "url"
-  },
-  {
-    "Title": "Zabriskie Point",
-    "director": {
-      "Name": "Michelangelo Antonioni",
-      "Birth Date": 1912,
-    },
-    "Genre": {
-      "Name": "Drama",
-      "description": "description"
-    },
-    "ImageURL": "https://m.media-amazon.com/images/I/51EwYqplVjL._SX300_SY300_QL70_ML2_.jpg"
-  },
-  {
-    "Title": "Crimson Peak",
-    "director": {
-      "Name": "Guillermo del Toro",
-      "Birth Date": 1964
-    },
-    "Genre": {
-      "Name": "Drama",
-      "description": "description"
-    },
-    "ImageURL": "url"
-  }
-];
+//import auth into index
 
-let users = [
-  {    
-    "name": "Maddie",
-    "favoriteMovies": ["Stealing Beauty"],
-    "id": 1,
-  },
-  {
-    "name": "Lee",
-    "favoriteMovies": ["Persona"],
-    "id": 5,
-  }
-]
+// const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+// morgan = require("morgan");
 
 let myLogger = (req, res, next) => {
   console.log(req.url);
@@ -153,139 +35,157 @@ let requestTime = (req, res, next) => {
   next();
 };
 
+// Users
+//   .create({
+//     name: req.body.name,
+//     email: req.body.email,
+//     favoriteMovies: req.body.favoriteMovies
+//   })
+
 app.use(myLogger);
 app.use(requestTime);
 app.use(morgan("common"));
 
-
-// GET requests
-app.get('/', (req, res) => {
-  let responseText = 'Welcome to my movie club';
-  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-  res.send(responseText);
+//default text response when at /
+app.get("/", (req, res) => {
+  res.send("Welcome!");
 });
 
-app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', { root: __dirname });
+//return JSON object when at /movies
+app.get("/movies", (req, res) => {
+  Movies.find()
+  .then((movies) => {
+    res.status(201).json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
 });
 
-app.get('/movies', (req, res) => {
-  res.json(movies);
-  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-  res.send(responseText);
+// get a movie by the title
+app.get('/movies/title/:Title', (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-app.get('/topmovies', (req, res) => {
-  res.sendFile('public/ten_movies.html', { root: __dirname });
+//get a director by name
+app.get('/movies/directors/:DirectorName', (req, res) => {
+  Movies.findOne({ "Director.Name": req.params.DirectorName})
+    .then((directors) => {
+      res.json(directors);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-//Title
-app.get('/movies/:title', (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find(movie => movie.Title === title);
+//get all users
+app.get("/users", function (req, res) {
+  Users.find()
+    .then(function(users) {
+      res.status(201).json(users);
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
-  if (movie) {
-    res.status(200).json(movie);
+// allow users to register
+app.post ('/users', (req, res) => {
+  console.log(333333, users)
+  Users.findOne({ name: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + "already exists")
+      } else {
+        Users.create({
+          "name": req.body.Username,
+          "email": req.body.Email,
+      })
+    .then((user) => {
+    res.status(201).json(user);
+  })
+  .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
   }
-  else {
-    res.status(400).send("no such movie")
-  }
+})
+})
+
+
+//   console.log(777777777777, req.body)
+//   const userData = req.body;
+//   const user = new Users(userData);
+
+//   user.save()
+//     .then(() => {
+//       res.status(201).json({ message: 'User saved successfully' });
+//     })
+//     .catch(error => {
+//       res.status(500).json({ error: 'An error occurred while saving the user' });
+//     });
+
+// // }
+// // );
+
+//get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ name: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-//Genre 
-app.get('/movies/genre/:genreName', (req, res) => {
-  const { genreName } = req.params;
-  const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
-
-  if (genre) {
-    res.status(200).json(genre);
-  }
-  else {
-    res.status(400).send("no such genre")
-  }
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ name: req.params.Username }, {
+    $push: { FavoriteMovies: req.params.MovieID }
+  },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
 });
 
-//DirectorsName
-app.get('/movies/directors/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = movies.find(movie => movie.director.Name === directorName).director;
-
-  if (director) {
-    res.status(200).json(director);
-  }
-  else {
-    res.status(400).send("no such director")
-  }
+// Delete a user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ name: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-//Add NewUser
-app.post('/users', (req, res) => {
-  let newUser = req.body;
-
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);    
-  } else {
-    res.status(400).send("nmu");
-  }
-});
-
-//Update
-app.put('/users/:userid', (req, res) => {
-  const { id } = req.params;
-  const updatedUser = req.body;
-
-  let user = users.find( user => user.id == id);
-  if (user) {
-    user.name = updatedUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send("no name here");
-  }
-});
-
-//ADD A MOVIE (POST)
-app.post('/users/:id/favoriteMovies/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-
-  let user = users.find(user => user.id === +id);
-  if (user) {
-    user.favoriteMovies.push.movieTitle;
-    res.status(200).send(`${movieTitle} has been added to user ${id} array`);
-  } else {
-    res.status(400).send("no such user");
-  }
-});
-
-//Delete Movie
-app.delete('/users/:id/favoriteMovies/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-
-  let user = users.find(user => user.id === +id);
-  if (user) {
-    user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
-    res.status(200).send(`${movieTitle} has been removed from user ${id} array`);
-  } else {
-    res.status(400).send("no such movie");
-  }
-});
-
-//Delete (deregister)
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
-
-  let user = users.find(user => user.id == id);
-  if (user) {
-    users = users.filter(user => user.id != id);
-    res.status(200).send(`user ${id} has been deleted`);
-  } else {
-    res.status(400).send("no such user");
-  }
-});
-
+//access documentation.html using express.static
 app.use('/documentation', express.static('public'));
 
+//err handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Error!');
@@ -294,4 +194,4 @@ app.use((err, req, res, next) => {
 // listen for requests
 app.listen(8080, () => {
   console.log('Listening on port 8080.');
-});
+})
