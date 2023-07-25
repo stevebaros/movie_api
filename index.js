e5a9be20cd762dcd500e6a0d5b677fb79fd2c477
Eva Nikolaieva <draculahgirl@gmail.com>
2023-07-25 22:32:29 +0200

    update

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-// mongoose.connect('mongodb://localhost:27017/cfDB'),
-mongoose.connect(process.env.CONNECTION_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/cfDB'),
+// mongoose.connect(process.env.CONNECTION_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true };
 
   const app = express();
 
